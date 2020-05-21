d4830c236dfceeda8e8353415a7031bfd052439e
Diego Armando Villalobos Villalta <39570306+dvillalobos16@users.noreply.github.com>
2020-05-21 16:50:20 -0600

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,5 +2,5 @@ import { serve } from "https://deno.land/std@0.50.0/http/server.ts";
 const s = serve({ port: 8000 });
 console.log("http://localhost:8000/");
 for await (const req of s) {
-    req.respond({ body: "Hola mundo\n" });
+    req.respond({ body: "Hola world\n" });
 }
